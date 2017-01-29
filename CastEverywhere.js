ac3c67500b388fc427388319189dce7b909bb1e9
JimmySevere <JimmySevere@users.noreply.github.com>
2017-01-29 13:01:21 +0000

    Update CastEverywhere.js

--- CastEverywhere.js
+++ CastEverywhere.js
@@ -5,111 +5,112 @@
 // @description  Cast all video tag sources
 // @author       Bop
 // @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @require      https://www.gstatic.com/cv/js/sender/v1/cast_sender.js
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
 (function($) {
-    'use strict';
+  'use strict';
 
-    function runCastSetup(){
-        var bopCast = {};
+  //font awesome
+  $("head").prepend('<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">');
 
-        window.__onGCastApiAvailable = function(loaded, errorInfo) {
-            if (loaded) {
-                initializeCastApi();
-            } else {
-                console.log(errorInfo);
-            }
-        };
-
-        function errorFn(e){
-            console.log(e);
-        }
-
-        function receiverListener(e) {
-            if( e === chrome.cast.ReceiverAvailability.AVAILABLE) {
-            }
-        }
-
-        function sessionListener(session) {
-            bopCast.session = session;
-            if (session.media.length !== 0) {
-                onMediaDiscovered('onRequestSessionSuccess', session.media[0]);
-            }
-            if (session.status !== chrome.cast.SessionStatus.CONNECTED) {
-                console.log('SessionListener: Session disconnected');
-                // Update local player to disconnected state
-            }
-        }
-
-        function stopApp() {
-            if(bopCast.session)
-                bopCast.session.stop(errorFn, errorFn);
-        }
-
-        function initializeCastApi() {
-            var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
-            var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
-                                                      sessionListener,
-                                                      receiverListener);
-            chrome.cast.initialize(apiConfig, function(){
-            }, errorFn);
-        }
-
-        //font awesome
-        $("head").prepend('<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">');
-
-        $('video, audio').each(function(){
-            var video = this;
-            var castControls = $(''+
-                                 '<div class="bop-video-controls">'+
-                                 '<div class="close"><i class="fa fa-times" aria-hidden="true"></i></div>'+
-                                 '<div class="play"><i class="fa fa-play" aria-hidden="true"></i></div>'+
-                                 '<div class="pause"><i class="fa fa-pause" aria-hidden="true"></i></div>'+
-                                 '<div class="cast"><i class="fa fa-television" aria-hidden="true"></i></div>'+
-                                 '</div>');
-            castControls.css({fontFamily: 'FontAwesome', background: 'black', position: 'fixed', zIndex: '9999999', fontSize: '50px', top:'50px'})
-                .insertAfter(video);
-
-            castControls.find('div').css({display: 'inline-block', padding: '5px', color: 'white'});
-
-            $(video).detach();
-
-            castControls.off('click').on('click', '.close', function(){
-                $(video).insertBefore(castControls);
-                castControls.remove();
-            }).on('click', '.cast', function(e){
-                function onRequestSessionSuccess(session) {
-                    bopCast.session = session;
-                    var mediaInfo = new chrome.cast.media.MediaInfo(video.currentSrc);
-                    var request = new chrome.cast.media.LoadRequest(mediaInfo);
-                    function onMediaDiscovered(how, media) {
-                        bopCast.media = media;
-                        bopCast.video = video;
-                        //media.addUpdateListener(onMediaStatusUpdate);
-                        media.play();
-                    }
-                    session.loadMedia(request,
-                                      onMediaDiscovered.bind(this, 'loadMedia'),
-                                      errorFn);
-
-                }
-
-                chrome.cast.requestSession(onRequestSessionSuccess, errorFn);
-            }).on('click', '.pause', function(e){
-                bopCast.media.pause();
-            }).on('click', '.play', function(e){
-                bopCast.media.play();
-            });
-        });
-        $.getScript('https://www.gstatic.com/cv/js/sender/v1/cast_sender.js');
+  window.__onGCastApiAvailable = function(loaded, errorInfo) {
+    if (loaded) {
+      bopCast.init();
+    } else {
+      bopCast.error(errorInfo);
     }
+  };
+  
+  var bopCast = {
+    receiverID: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
+    list: {},
+    _list: $(),
+    controls: $(
+      '<div class="bop-video-controls">'+
+      '<div class="close"><i class="fa fa-times" aria-hidden="true"></i></div>'+
+      '<div class="list"><select></select></div>'+
+      '<div class="cast"><i class="fa fa-television" aria-hidden="true"></i><i class="fa fa-plus" aria-hidden="true"></i></div>'+
+      '</div>'
+    ),
+    updateList: function(){
+      this.list = {'Video':$('video').clone(), 'Audio':$('audio').clone()};
+      this._list = $();
+      
+      var listHTML = $();
+      $.each(this.list, function(type,$items){
+        this._list.add($items);
+        var og = $('<optgroup label="'+type+'"></optgroup>');
+        $items.each(function(i){
+          var label = this.title || this.alt || this.currentSrc || this.src;
+          og.append('<option value="'+type+'.'+i+'">'+label+'</option>');
+        });
+        listHTML.add(og);
+      });
+      this.controls.find('.list select').html(listHTML);
+    },
+    getSelectedMedia: function(){
+      var val = this.controls.find('.list select').val().split('.');
+      this.selectedMediaType = val[0];
+      this.selectedMedia = this.list[val[0]][val[1]];
+      return this.selectedMedia;
+    },
+    init: function(){
+      var session = cast.framework.CastContext.getInstance().getCurrentSession();
+      if( session.displayName !== 'Bop CastEverywhere' ){
+        var sessionRequest = new chrome.cast.SessionRequest(this.receiverID);
+        var apiConfig = new chrome.cast.ApiConfig(sessionRequest, this.sessionListener, this.receiverListener);
+        chrome.cast.initialize(apiConfig, function(){}, errorFn);
+      }
+    },
+    end: function(){
+      if(this.session)
+        this.session.stop(errorFn, errorFn);
+    },
+    error: function(e){
+      console.log(e);
+    },
+    receiverListener: function(e) {
+      if( e === chrome.cast.ReceiverAvailability.AVAILABLE) {
+      }
+    },
+    sessionListener: function(session) {
+      if (session.media.length !== 0) {
+        onMediaDiscovered('onRequestSessionSuccess', this.session.media[0]);
+      }
+      if (session.status !== chrome.cast.SessionStatus.CONNECTED) {
+        console.log('SessionListener: Session disconnected');
+      }
+    }
+  };
+    
+  bopCast.controls.on('click', '.cast', function(e){
+    chrome.cast.requestSession(function(session){
+      bopCast.session = session;
+      var media = bopCast.getSelectedMedia();
+      var mediaSrc = media.currentSrc || media.src;
+      var mediaInfo = new chrome.cast.media.MediaInfo(mediaSrc);
+      var request = new chrome.cast.media.LoadRequest(mediaInfo);
+      function onMediaDiscovered(how, media) {
+        bopCast.media = media;
+        bopCast.video = video;
+        //media.addUpdateListener(onMediaStatusUpdate);
+        media.play();
+      }
+      session.loadMedia(request, onMediaDiscovered.bind(this, 'loadMedia'), bopCast.error);
 
-    var iid = window.setInterval(function(){
-        if($('video').length){
-            runCastSetup();
-            window.clearInterval(iid);
-        }
-    }, 5000);
+    }, errorFn);
+  })
+  .css({fontFamily: 'FontAwesome', background: 'black', position: 'fixed', zIndex: '9999999', fontSize: '50px', top:'50px'})
+  .find('div').css({display: 'inline-block', padding: '5px', color: 'white'});
+
+  var iid = window.setInterval(function(){
+    bopCast.updateList();
+    if(bopCast._list.length){
+      runCastSetup();
+      window.clearInterval(iid);
+    }
+  }, 5000);
 })(jQuery);
